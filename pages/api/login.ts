2568c3eeb142f2058a191ca8b2e476d4ab445891
DeccaSoft <andremazevedo@gmail.com>
2022-10-12 12:45:44 -0300

    Iniciando JWT

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,15 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+import type {LoginResposta} from '../../types/LoginResposta';
+import md5 from "md5";
+import { UsuarioModel } from "../../models/UsuarioModel";
+import jwt from 'jsonwebtoken';
 
-const endpointLogin = (
+
+const endpointLogin = async (
     req: NextApiRequest,
-    res: NextApiResponse<RespostaPadraoMsg>
+    res: NextApiResponse<RespostaPadraoMsg | LoginResposta>   //res é uma resposta padrão ou qualquer coisa! Depois any será transformado em um objeto... e foi substituído por LoginResposta
 ) => {
+    const {MINHA_CHAVE_JWT} = process.env;
+    if(!MINHA_CHAVE_JWT){
+        return res.status(500).json({erro: 'ENV JWT NÃO Informada!'});
+    }
+
     if(req.method === 'POST'){
-        const {login, senha} = req.body;
-        if(login === 'admin@admin.com' && senha === 'Admin@123'){
-            return res.status(200).json({msg: 'Usuário Autenticado com Sucesso!'});
+        const {login, senha} = req.body;    //Destructor (Desconstroi um objeto)
+        const usuariosEncontrados = await UsuarioModel.find({email: login, senha: md5(senha)});
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            const usuarioEncontrado = usuariosEncontrados[0];
+
+            const token = jwt.sign({_id : usuarioEncontrado._id}, MINHA_CHAVE_JWT);
+            //return res.status(200).json({msg: `Usuário ${usuarioEncontrado.nome} Autenticado com Sucesso!`});
+            return res.status(200).json({
+                nome: usuarioEncontrado.nome, 
+                email: usuarioEncontrado.email, 
+                token});
+
         }
         return res.status(400).json({erro: 'Usuário e/ou Senha Inválido(s)!'});
     }
