cea7c71024a09f3c4a506902d2de4ef83738a321
DeccaSoft <andremazevedo@gmail.com>
2022-11-02 09:47:45 -0300

    API like deslike

---
+++ pages/api/like.ts
@@ -0,0 +1,44 @@
+import { UsuarioModel } from './../../models/UsuarioModel';
+import { PublicacaoModel } from './../../models/PublicacaoModel';
+import { conectarMongoDB } from './../../middlewares/conectarMongoDB';
+import { validarTokenJwt } from './../../middlewares/validarTokenJwt';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+
+const likeEndpoint =async (req:NextApiRequest, res:NextApiResponse<RespostaPadraoMsg>) => {
+    try {
+        //Id da Publicação
+        if(req.method === 'PUT'){
+            const {id} = req?.query;
+            const publicacao = await PublicacaoModel.findById(id);
+            if(!publicacao){
+                res.status(400).json({erro : 'Publicação NÃO Encontrada!'});
+            }
+
+            //Id do Usuário que está Curtindo a Publicação
+            const {userId} = req?.query;
+            const usuario = await UsuarioModel.findById(userId);
+            if(!usuario){
+                return res.status(400).json({erro : 'Usuário NÃO Encontrado!'});
+            }
+
+            //Index = -1 => Ele NÃO Curte a Foto, Index > -1 => Ele Já Curte a Foto
+            const IndexDoUsuarioNoLike = publicacao.likes.findIndex((e : any) => e.toString() === usuario._id.toString());
+            if(IndexDoUsuarioNoLike != -1){
+                publicacao.likes.splice(IndexDoUsuarioNoLike, 1) // .splice() remove 1 index da lista
+                await PublicacaoModel.findByIdAndUpdate({_id : publicacao._id}, publicacao);
+                return res.status(200).json({msg : 'Publicacao Descurtida com Sucesso!'});
+            }else{
+                publicacao.likes.push(usuario._id) // .push() coloca no final da lista
+                await PublicacaoModel.findByIdAndUpdate({_id : publicacao._id}, publicacao);
+                return res.status(200).json({msg : 'Publicacao Curtida com Sucesso!'});
+            }
+        }
+        return res.status(405).json({erro : 'Método Informado NÃO é Válido!'});
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({erro : 'Ocorreu um Erro ao Curtir/Descurtir a Publicação!'});      
+    }
+}
+
+export default validarTokenJwt(conectarMongoDB(likeEndpoint));
--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -7,17 +7,26 @@ import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 const pesquisaEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any[]>) => {
     try {
         if(req.method === 'GET'){
-            const{filtro} = req.query;    //Body só se usa no Post e no Put
-            if(!filtro || filtro.length < 2){
-                return res.status(400).json({erro : 'Filtro para Busca Inválido! A Busca Deve ter pelo menos 2 Caracteres.'});
-            }
+            if(req?.query?.id){
+                const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id);
+                if(!usuarioEncontrado){
+                    return res.status(400).json({erro : 'Usuário NÃO Encontrado!'});
+                }
+                usuarioEncontrado.senha = null;
+                return res.status(200).json(usuarioEncontrado);
+            }else{
+                const{filtro} = req.query;    //Body só se usa no Post e no Put
+                if(!filtro || filtro.length < 2){
+                    return res.status(400).json({erro : 'Filtro para Busca Inválido! A Busca Deve ter pelo menos 2 Caracteres.'});
+                }
 
-            const usuariosEncontrados = await UsuarioModel.find({
-                //Pesquisa por nome ou E-mail
-                $or: [{nome : {$regex : filtro, $options: 'i'}},   //regex = .contains e optios:i = Ignore Case
-                    {email : {$regex : filtro, $options : 'i'}}]
-            });
-            return res.status(200).json(usuariosEncontrados);
+                const usuariosEncontrados = await UsuarioModel.find({
+                    //Pesquisa por nome ou E-mail
+                    $or: [{nome : {$regex : filtro, $options: 'i'}},   //regex = .contains e optios:i = Ignore Case
+                        {email : {$regex : filtro, $options : 'i'}}]
+                });
+                return res.status(200).json(usuariosEncontrados);
+            }            
         }
         return res.status(405).json({erro : 'Método informado NÃO Inválido'});
     } catch (e) {
