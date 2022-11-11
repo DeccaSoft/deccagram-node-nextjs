ddc61aaa9e3e594edba555d33da68b734e1cad87
DeccaSoft <andremazevedo@gmail.com>
2022-11-11 13:26:00 -0300

    comentários

---
+++ pages/api/comentario.ts
@@ -0,0 +1,44 @@
+import { UsuarioModel } from './../../models/UsuarioModel';
+import { conectarMongoDB } from './../../middlewares/conectarMongoDB';
+import { validarTokenJwt } from './../../middlewares/validarTokenJwt';
+import { RespostaPadraoMsg } from './../../types/RespostaPadraoMsg';
+import {NextApiRequest, NextApiResponse} from 'next';
+import { PublicacaoModel } from '../../models/PublicacaoModel';
+
+const comentarioEndpoint = async (req:NextApiRequest, res:NextApiResponse<RespostaPadraoMsg>) => {
+    try {
+        if(req.method === 'PUT'){
+            //Usuário e Publicação vem do Query
+            const {userId, id} = req.query;
+            const usuarioLogado = await UsuarioModel.findById(userId);
+            if(!usuarioLogado){
+                return res.status(400).json({erro: 'Usuário NÃO Encontrado!'});
+            }
+
+            const publicacao = await PublicacaoModel.findById(id);
+            if(!publicacao){
+                return res.status(400).json({erro: 'Publicação NÃO Encontrada!'});
+            }
+
+            //Comentário vem do Body
+            if(!req.body || !req.body.comentario){
+                return res.status(400).json({erro: 'Comentário Inválido!'});
+            }
+            const comentario = {
+                usuarioId : usuarioLogado._id,
+                nome : usuarioLogado.nome,
+                comentario : req.body.comentario
+            }
+            publicacao.comentarios.push(comentario);
+            await PublicacaoModel.findByIdAndUpdate({_id : publicacao._id}, publicacao);
+            return res.status(200).json({msg: 'Comentário Adicionado com Sucesso!'});
+        }
+        return res.status(405).json({erro: 'O Método Informado NÃO é Válido!'});
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({erro: 'Ocorreu um Erro ao Adicionar o Camentário!'});
+        
+    }
+}
+
+export default validarTokenJwt(conectarMongoDB(comentarioEndpoint));
